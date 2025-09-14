b94f1a1e29d95fcf70f4f9377b3f408fcfc80b49
smail-yazidi <smail.yazidi.work@gmail.com>
2025-09-14 22:11:24 +0100

    add README.md

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,10 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
+
+
+
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
